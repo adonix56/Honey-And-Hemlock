cab1b397a4bf2843a9f87ddc886fd3233faeee3b
Justin Salanga <justin.salanga@gmail.com>
2026-01-26 23:55:30 -0800

    Added Drippable Basics and StarlightDrop

---
+++ Assets/Scripts/Ingredients/DrippableIngredient.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+namespace HoneyAndHemlock.Ingredients
+{
+    public class DrippableIngredient : XRGrabInteractable, IIngredientSource
+    {
+        [Header("Ingredient Configuration")]
+        [SerializeField] private IngredientSO _data;
+
+        public IngredientType IngredientType => _data != null ? _data.Type : default;
+        public IngredientSO Data => _data;
+        public IngredientDeliveryMethod DeliveryMethod => IngredientDeliveryMethod.Drop;
+    }
+}
